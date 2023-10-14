11d6805511e826a5a6439bc477ce6df95998c2bc
Bobby Kuepper <robertkuepper@gmail.com>
2023-10-14 11:08:22 -0500

    lab for index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout() {
+    console.log("Hello" .toUpperCase());
+}logShout();
+function logWhisper() {
+    console.log("Hello" .toLowerCase());
+}logWhisper();
+function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase() === "let's have dinner together!") {
+        return "I would love to!";
+    } else if (string === string.toUpperCase()) {
+        return "YES INDEED!";
+    } else if (string === string.toLowerCase()) {
+        return "I can't hear you!";
+    }
+}
+function sayHiToHeadphonedRoommate(word){
+    if (word.toLowerCase() === "let's have dinner together!")
+}
